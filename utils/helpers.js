002fc782a1dd31fd158f5389b53c8f7da4820407
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-11-14 12:14:56 -0400

    Defined Handlebars Helpers and Authentication Middleware

---
+++ utils/helpers.js
@@ -0,0 +1,8 @@
+// utils/helpers.js
+module.exports = {
+    format_date: date => {
+      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+    },
+    // Other helpers can be added here
+  };
+  
