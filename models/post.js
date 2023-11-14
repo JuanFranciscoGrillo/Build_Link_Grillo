7f831d699691bd1d5e4fcbbff82dfd64b774dc4e
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-11-14 09:15:34 -0400

    Generated Sequelize models and defined associantions on post model

--- models/post.js
+++ models/post.js
@@ -1,7 +1,6 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Post extends Model {
     /**
@@ -10,18 +9,37 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      // Post to User (Many-to-One)
+      Post.belongsTo(models.User, {
+        foreignKey: 'user_id',
+        onDelete: 'CASCADE'
+      });
+
+      // Post to Comments (One-to-Many)
+      Post.hasMany(models.Comment, {
+        foreignKey: 'post_id',
+        onDelete: 'CASCADE'
+      });
+
+      // Post to Applications (One-to-Many)
+      Post.hasMany(models.Application, {
+        foreignKey: 'post_id',
+        onDelete: 'CASCADE'
+      });
     }
   }
+
   Post.init({
     title: DataTypes.STRING,
     description: DataTypes.TEXT,
     location: DataTypes.STRING,
     salary: DataTypes.INTEGER,
-    company: DataTypes.STRING
+    company: DataTypes.STRING,
+    user_id: DataTypes.INTEGER // assuming this is the foreign key to User
   }, {
     sequelize,
     modelName: 'Post',
   });
+
   return Post;
 };
