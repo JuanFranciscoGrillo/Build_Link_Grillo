f3b9be5506ea66d81679dfe3afacb32cc04cb4b7
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-11-14 13:45:09 -0400

    Defined Handlebars

---
+++ server.js
@@ -0,0 +1,39 @@
+// server.js
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Set up Handlebars.js engine
+const hbs = exphbs.create({ helpers: require('./utils/helpers') });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Set up session with sequelize store
+app.use(session({
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+}));
+
+// Turn on routes
+app.use(routes);
+
+// Sync sequelize models to the database, then start the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
