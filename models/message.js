1cb38c73e1707563e04299b5fc551964aea07170
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-11-14 09:18:49 -0400

    Generated Sequelize models and defined associantions on message  model

--- models/message.js
+++ models/message.js
@@ -1,7 +1,6 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Message extends Model {
     /**
@@ -10,17 +9,43 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      // Message to User (Many-to-One) for Sender
+      Message.belongsTo(models.User, {
+        foreignKey: 'senderId',
+        as: 'Sender'
+      });
+
+      // Message to User (Many-to-One) for Receiver
+      Message.belongsTo(models.User, {
+        foreignKey: 'receiverId',
+        as: 'Receiver'
+      });
     }
   }
+
   Message.init({
-    senderId: DataTypes.INTEGER,
-    receiverId: DataTypes.INTEGER,
+    senderId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'id'
+      }
+    },
+    receiverId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'id'
+      }
+    },
     content: DataTypes.TEXT,
     timestamp: DataTypes.DATE
   }, {
     sequelize,
     modelName: 'Message',
   });
+
   return Message;
 };
